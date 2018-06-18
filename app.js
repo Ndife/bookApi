46a3d5de25015ae14008e0cdbfcb3cad9048addd
Ndife Uchenna <stilesndife@gmail.com>
2018-06-18 19:03:05 +0100

    made some changes: book is adding perfectly and i also have a function getbooks and getbookbyid all working perfectly

--- app.js
+++ app.js
@@ -19,11 +19,28 @@ Library.prototype.getLibrary = function (){
 Library.prototype.updatedLibrary = function (){
     return fs.writeFileSync('./data.json',JSON.stringify(this.books)); 
 }
+
 Library.prototype.addBook = function(book){
-this.books.push(book);
-this.updatedLibrary();
+this.books = this.getLibrary();
+cbook = this.books.filter((b)=>book.id===b.id);
+    if(cbook==false){
+    this.books.push(book);
+    this.updatedLibrary();
+    }else{
+        console.log('book already exist with the ID');
+    }
 }
 
+Library.prototype.getBooks = function(){
+    return this.getLibrary();
+}
+
+Library.prototype.getBookById = function (id){
+    this.books = this.getLibrary();
+    let book = this.books.filter((book)=>book.id==id);
+    return book;
+}
 const book1 = new Book('kyle','may',2050,11);
 const lib = new Library("Ndife's");
 lib.addBook(book1);
+//  console.log(lib.getBooks());
